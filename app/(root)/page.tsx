f19001c6d544f69b3824330ae366bae2b12e31cc
Your Name <you@example.com>
2024-06-21 18:23:29 +0700

    add left side for pc and mobile

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,28 @@
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Truong'};
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting" 
+            title="Welcome"
+            user ={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions with ease."
+            />
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+            />
+        </header>
+
+      </div>
+    </section>
   )
 }
 
